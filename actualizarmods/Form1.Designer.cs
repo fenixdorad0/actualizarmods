2c8bebe8fd3aa5a2fbbc746aa6754fc8419d4b33
fenix3090 <fenix3090@FENIX3090>
2023-12-11 17:59:23 -0500

    Agregar archivos de proyecto.

---
+++ actualizarmods/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace actualizarmods
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonDescargar = new Button();
+            buttonIniciarServer = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            buttonObtenerIP = new Button();
+            SuspendLayout();
+            // 
+            // buttonDescargar
+            // 
+            buttonDescargar.Location = new Point(12, 79);
+            buttonDescargar.Name = "buttonDescargar";
+            buttonDescargar.Size = new Size(75, 23);
+            buttonDescargar.TabIndex = 0;
+            buttonDescargar.Text = "Descargar";
+            buttonDescargar.UseVisualStyleBackColor = true;
+            buttonDescargar.Click += button1_Click;
+            // 
+            // buttonIniciarServer
+            // 
+            buttonIniciarServer.Location = new Point(93, 79);
+            buttonIniciarServer.Name = "buttonIniciarServer";
+            buttonIniciarServer.Size = new Size(75, 23);
+            buttonIniciarServer.TabIndex = 1;
+            buttonIniciarServer.Text = "Iniciar servidor";
+            buttonIniciarServer.UseVisualStyleBackColor = true;
+            buttonIniciarServer.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Que esta pasando";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 20);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Eres el:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 125);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Notas de version1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(271, 30);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "192.168.0.200";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(271, 59);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 5;
+            textBox2.Text = "8080";
+            // 
+            // buttonObtenerIP
+            // 
+            buttonObtenerIP.Location = new Point(271, 88);
+            buttonObtenerIP.Name = "buttonObtenerIP";
+            buttonObtenerIP.Size = new Size(100, 23);
+            buttonObtenerIP.TabIndex = 6;
+            buttonObtenerIP.Text = "Configurar IP";
+            buttonObtenerIP.UseVisualStyleBackColor = true;
+            buttonObtenerIP.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(441, 160);
+            Controls.Add(buttonObtenerIP);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(buttonIniciarServer);
+            Controls.Add(buttonDescargar);
+            Name = "Form1";
+            Text = "Launcher by fenixdorad0";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonDescargar;
+        private Button buttonIniciarServer;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button buttonObtenerIP;
+    }
+}
---
+++ actualizarmods/Form1.cs
@@ -0,0 +1,202 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Net;
+using System.Net.Sockets;
+
+
+
+
+namespace actualizarmods
+{
+    public partial class Form1 : Form
+    {
+        //private string carpetaMods = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".minecraft", "mods");
+        private string carpetaMods = @"F:\server youtube mods y plugin test\mods";
+        private string direccionIP = "192.168.0.200";
+        private int puerto = 9090;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        static void GenerarConfiguracion(string carpetaMods)
+        {
+            List<ModInfo> mods = new List<ModInfo>();
+
+            foreach (string archivoMod in Directory.GetFiles(carpetaMods, "*.jar"))
+            {
+                mods.Add(new ModInfo { Name = Path.GetFileName(archivoMod) });
+            }
+
+            Configuracion configuracion = new Configuracion { Mods = mods };
+
+            string jsonConfiguracion = Newtonsoft.Json.JsonConvert.SerializeObject(configuracion, Newtonsoft.Json.Formatting.Indented);
+
+            File.WriteAllText("configActualizarmods.json", jsonConfiguracion);
+        }
+
+        class Configuracion
+        {
+            public List<ModInfo> Mods { get; set; }
+        }
+
+        class ModInfo
+        {
+            public string Name { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Descargando archivo por favor espere...";
+            DescargarArchivos();
+            label1.Text = "Archivos descargados";
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string miDireccionIP = ObtenerDireccionIP();
+
+            if (miDireccionIP == "192.168.0.200")
+            {
+                label2.Text = "Eres el: servidor";
+
+                label1.Text = "iniciando Servidor porfavor espere";
+                IniciarServidorHttp(); // Iniciar el servidor HTTP cuando se carga el formulario
+                label1.Text = "Servidor iniciado";
+            }
+            else
+            {
+                label2.Text = "Eres el: eres el cliente usa el otro boton";
+
+            }
+
+
+        }
+
+        static string ObtenerDireccionIP()
+        {
+            string direccionIP = string.Empty;
+
+            try
+            {
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+                {
+                    socket.Connect("8.8.8.8", 65530); // Conéctate a un servidor externo
+                    IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                    direccionIP = endPoint.Address.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al obtener la dirección IP: {ex.Message}");
+            }
+
+            return direccionIP;
+        }
+
+        private void DescargarArchivos()
+        {
+            string[] archivos = Directory.GetFiles(carpetaMods, "*.jar");
+
+            foreach (string archivo in archivos)
+            {
+                string nombreArchivo = Path.GetFileName(archivo);
+                string urlDescarga = $"http://{direccionIP}:{puerto}/{nombreArchivo}";
+                string rutaGuardado = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), nombreArchivo);
+                //string rutaGuardado = @"C:\Users\fenix3090\Downloads\prueba2\" + nombreArchivo;
+
+                using (WebClient cliente = new WebClient())
+                {
+                    try
+                    {
+                        cliente.DownloadFile(urlDescarga, rutaGuardado);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void IniciarServidorHttp()
+        {
+            string url = $"http://{direccionIP}:{puerto}/";
+
+            // Inicia el servidor HTTP en un hilo separado
+            Task.Run(() =>
+            {
+                using (HttpListener httpListener = new HttpListener())
+                {
+                    httpListener.Prefixes.Add(url);
+                    httpListener.Start();
+                    Console.WriteLine($"Servidor HTTP iniciado en {url}");
+
+                    // Espera solicitudes
+                    while (true)
+                    {
+                        HttpListenerContext context = httpListener.GetContext();
+                        HttpListenerResponse response = context.Response;
+
+                        // Obtiene el nombre del archivo de la URL
+                        string archivoSolicitado = Path.GetFileName(context.Request.Url.AbsolutePath);
+
+                        // Ruta completa del archivo solicitado
+                        string rutaArchivo = Path.Combine(carpetaMods, archivoSolicitado);
+
+                        if (File.Exists(rutaArchivo))
+                        {
+                            // Configura el tipo de contenido binario
+                            response.ContentType = "application/octet-stream";
+
+                            using (Stream output = response.OutputStream)
+                            {
+                                // Lee el archivo solicitado y envíalo como respuesta
+                                byte[] contenido = File.ReadAllBytes(rutaArchivo);
+                                output.Write(contenido, 0, contenido.Length);
+                            }
+                        }
+                        else
+                        {
+                            // Enviar una respuesta de error si el archivo no se encuentra
+                            response.StatusCode = 404;
+                        }
+                        response.Close();
+                    }
+                }
+            });
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                direccionIP = textBox1.Text;
+                puerto = int.Parse(textBox2.Text);
+                MessageBox.Show(direccionIP);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(direccionIP+":"+puerto);
+        }
+    }
+}
